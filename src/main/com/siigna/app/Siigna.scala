ff5e0a9c31a012f18c4efe71fc12b88b24e39972
Jegp <jensep@gmail.com>
2012-03-24 10:21:10 +0100

    Incremented version number to 0.2 (new model)!

--- src/main/com/siigna/app/Siigna.scala
+++ src/main/com/siigna/app/Siigna.scala
@@ -74,7 +74,7 @@ object Siigna extends Interface {
   /**
    * The version of Siigna.
    */
-  val version = "v. 0.1.15.4"
+  val version = "v. 0.2.0"
 
   /**
    * The zoom-speed. Defaults to 0.1 (i. e. 10%).
