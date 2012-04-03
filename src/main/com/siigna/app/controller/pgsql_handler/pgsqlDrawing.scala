90768532fb7293ef4db9bb1508993fc500db6b18
Jegp <jensep@gmail.com>
2012-04-03 14:06:01 +0200

    Fixed a bug

--- src/main/com/siigna/app/controller/pgsql_handler/pgsqlDrawing.scala
+++ src/main/com/siigna/app/controller/pgsql_handler/pgsqlDrawing.scala
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2012. Siigna is released under the creative common license by-nc-sa. You are free
+ * to Share — to copy, distribute and transmit the work,
+ * to Remix — to adapt the work
+ *
+ * Under the following conditions:
+ * Attribution —  You must attribute the work to http://siigna.com in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work).
+ * Noncommercial — You may not use this work for commercial purposes.
+ * Share Alike — If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
+ */
+
 package com.siigna.app.controller.pgsql_handler
 
 /**
@@ -23,7 +34,7 @@ class pgsqlDrawing {
     var createStatement: Statement = databaseConnection.createStatement()
 
     //Finder point-ids på de points, der er i shapen:
-    query =   "INSERT INTO drawing (drawing_name,drawing_offset_x,drawing_offset_y,drawing_offset_z) " +
+    var query = "INSERT INTO drawing (drawing_name,drawing_offset_x,drawing_offset_y,drawing_offset_z) " +
       "VALUES (" +  drawingName + "," + offsetX + "," + offsetY + "," + offsetZ + "RETURNING drawing_id"
 
     val queryResult: ResultSet = createStatement.executeQuery(query)
