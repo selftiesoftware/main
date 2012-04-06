3a67c12ce7bc39571d73f8ce5c1bdb6fe8febd35
unknown <Niels Egholm@NielsEgholm.(none)>
2012-04-06 16:21:24 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/com/siigna/app/controller/Control.scala

---
+++ src/main/com/siigna/app/model/drawing/activeDrawing.scala
@@ -0,0 +1,19 @@
+package com.siigna.app.model.drawing
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Niels Egholm
+ * Date: 06-04-12
+ * Time: 16:17
+ * To change this template use File | Settings | File Templates.
+ */
+
+object activeDrawing {
+
+  var drawingId: Option[Int] = None
+
+  def setId(id:Int) {
+    drawingId = id
+  }
+
+}
