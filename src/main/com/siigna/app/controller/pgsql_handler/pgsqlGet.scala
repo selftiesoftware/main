a533ad42d5fbd7d9327959756b793cc14f06c7a4
unknown <Niels Egholm@NielsEgholm.(none)>
2012-04-06 11:29:45 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/com/siigna/app/controller/Control.scala

--- src/main/com/siigna/app/controller/pgsql_handler/pgsqlGet.scala
+++ src/main/com/siigna/app/controller/pgsql_handler/pgsqlGet.scala
@@ -16,7 +16,7 @@ import java.sql._
 import com.siigna.app.model.shape._
 import com.siigna.util.geom.Vector2D
 
-object pgsqlGet {
+class pgsqlGet {
 
   //Returnerer map af id -> immutableshape
   def allShapesInDrawingFromDrawingIdWithDatabaseId (drawingId: Int) = {
