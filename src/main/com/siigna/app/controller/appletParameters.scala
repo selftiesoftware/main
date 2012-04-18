19574a93b0d5c37ee06a48cc63ab28b7f7512611
unknown <Niels Egholm@NielsEgholm.(none)>
2012-04-18 18:47:29 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/com/siigna/app/controller/Control.scala

--- src/main/com/siigna/app/controller/appletParameters.scala
+++ src/main/com/siigna/app/controller/appletParameters.scala
@@ -21,14 +21,14 @@ object AppletParameters {
   }
   
   def getParametersInt(parameterName: Option[String]) = {
-    var parameter: Option[Int] = None
+    var parameter: Option[String] = None
     if (applet.isDefined) {
       //Hvis appletten ikke er startet fra hjemmesiden kan der ikke hentes brugerid herfra - 1 indsættes.
       try { (parameter = Some(applet.get.getParameter(parameterName.get)))
       } catch {
         case e: java.lang.NullPointerException => {
           println("No contributor logged in at Siigna.com homepage. Setting user to Anonymous. If you are connected to the internet, you may log into the Siigna applet from the menu.")
-          parameter = Some (3)
+          parameter = Some ("anonymous")
         }
       }
     } else {
