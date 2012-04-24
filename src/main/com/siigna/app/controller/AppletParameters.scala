9a5d686b0f0c26f4e73524af71f6a4d8755be9de
Jegp <jensep@gmail.com>
2012-04-24 21:41:35 +0200

    Merged

--- src/main/com/siigna/app/controller/AppletParameters.scala
+++ src/main/com/siigna/app/controller/AppletParameters.scala
@@ -12,16 +12,40 @@
 package com.siigna.app.controller
 
 import java.applet.Applet
+import remote.{GetNewShapeIds, GetNewShapeId}
 
 object AppletParameters {
 
   private var applet : Option[Applet] = None
   var clientReference: Option[Client] = None
+  var shapeIdBank: Seq[Int] = Seq()
+  var drawingIdBank: Seq[Int] = Seq()
 
-  def setApplet (newApplet: Applet) { 
-    applet = Some(newApplet) 
+  /**
+   * Returnerer Client, der er gemt i variablen clientReference
+   * @return
+   */
+  def getClient = {
+    (clientReference.get)
   }
-  
+
+  /**
+   * Returnerer en shapeId fra "banken". Kontrollerer, hvor mange id'er, der er tilbage i banken.
+   * Hvis der er under et vist antal anmodes om "en ny portion".
+   * @return
+   */
+  def getNewShapeId = {
+    val shapeId = shapeIdBank.head
+    shapeIdBank = shapeIdBank.tail
+    if (shapeIdBank.length<2) GetNewShapeIds(2,com.siigna.app.controller.AppletParameters.getClient)
+    (shapeId)
+  }
+
+  /**
+   * Fremfinder parametre, der er strenge, der er sendt ved opstart af applet fra "param"-tags fra HTML
+   * @param parameterName
+   * @return
+   */
   def getParametersString(parameterName: String) = {
     var parameter: Option[String] = None
     if (applet.isDefined) {
@@ -39,6 +63,11 @@ object AppletParameters {
     (parameter)
   }
 
+  /**
+   * Fremfinder parametre, der er integers, der er sendt ved opstart af applet fra "param"-tags fra HTML
+   * @param parameterName
+   * @return
+   */
   def getParametersInt(parameterName: String) = {
     var parameter: Option[Int] = None
     if (applet.isDefined) {
@@ -46,7 +75,8 @@ object AppletParameters {
       try { (parameter = Some(applet.get.getParameter(parameterName).toInt))
       } catch {
         case e: java.lang.NullPointerException => {
-          println("No drawing Id provided from homepage.")
+          println("No drawing Id provided from homepage. Setting drawingId to 1.")
+          parameter = Some(1)
         }
       }
     } else {
@@ -55,10 +85,37 @@ object AppletParameters {
     (parameter)
   }
 
-  def getClient = {
-      (clientReference.get)
+  /**
+   * Saves a new shapeId into the shapeIdBank variable
+   * @param shapeId
+   */
+  def receiveNewShapeId(shapeId:Int) {
+    shapeIdBank = shapeIdBank :+ shapeId
+    println("shapeIdBank is now: "+shapeIdBank)
   }
 
+  /**
+   * Saves a sequence of new shapeIds into the shapeIdBank variable
+   * @param shapeIds
+   */
+  def receiveNewShapeIds(shapeIds:Seq[Int]) {
+    shapeIdBank = shapeIdBank ++ shapeIds
+    println("shapeIdBank is now: "+shapeIdBank)
+  }
+
+  /**
+   * Modtager en instans af "Applet", der har modtaget informationer
+   * fra "param"-tags fra HTML, når appletten startes af HTML-kode. Bruges ved opstart af applet.
+   * @param newApplet
+   */
+  def setApplet (newApplet: Applet) {
+    applet = Some(newApplet)
+  }
+
+  /**
+   * Gemmer Client i variablen clientReference
+   * @param newClient
+   */
   def setClient(newClient:Option[Client]) {
     clientReference = newClient
   }
