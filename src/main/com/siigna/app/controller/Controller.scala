396c166ad3e0f92a450f2b1eacbb00803125a870
unknown <Niels Egholm@NielsEgholm.(none)>
2012-04-26 07:08:41 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/com/siigna/app/controller/AppletParameters.scala

--- src/main/com/siigna/app/controller/Controller.scala
+++ src/main/com/siigna/app/controller/Controller.scala
@@ -127,13 +127,9 @@ object Controller extends Actor {
                   client = Some(r.client)
                   val id = client.get.id
                   Log.info("Controller registered client with id " + id)
+                  println("Registerdel kører")
                   //Gemmer klienten, der identificerer appletten overfor serveren, i AppletParameters
                   AppletParameters.setClient(client)
-                  //gets the active drawing, if one was selected at www.siigna.com, or None if none was received
-                  val requestDrawingId = com.siigna.app.controller.AppletParameters.getParametersInt("drawingId")
-                  //If one was received, it is set as active drawing. Otherwise a new drawing is created
-                  if (requestDrawingId.isDefined)
-                    com.siigna.app.controller.AppletParameters.setDrawingId(requestDrawingId.get)
                   //Hvis der er kommet en aktiv tegning fra hjemmesiden hentes den, ellers laves der en ny:
                   if (AppletParameters.readDrawingIdAsOption.isDefined) {
                     sink ! GetDrawing(AppletParameters.readDrawingIdAsOption.get, client.get)
@@ -155,7 +151,7 @@ object Controller extends Actor {
         }
 
         case command : NewDrawingId => {
-          AppletParameters.setDrawingId(command.retrieveNewDrawingId)
+          AppletParameters.setDrawingIdAndRegisterItWithTheServer(command.retrieveNewDrawingId)
         }
         case command : NewShapeId => {
           AppletParameters.receiveNewShapeId(command.retrieveNewShapeId)
