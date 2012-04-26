7baf8af2737740dd6eab4b3dd623d39de52adccc
unknown <Niels Egholm@NielsEgholm.(none)>
2012-04-26 07:08:41 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/com/siigna/app/controller/AppletParameters.scala

--- src/main/com/siigna/app/SiignaApplet.scala
+++ src/main/com/siigna/app/SiignaApplet.scala
@@ -70,8 +70,12 @@ class SiignaApplet extends Applet {
     //get the active user, if a log in was performed at www.siigna.com
     com.siigna.app.controller.AppletParameters.getContributorNameFromHomepage
 
-    // Henter info om aktiv bruger og aktiv tegning
-    //com.siigna.app.model.drawing.activeDrawing.getInfoOnUserAndDrawingAtStartup()
+    //gets the active drawing, if one was selected at www.siigna.com, or None if none was received
+    val requestDrawingId = com.siigna.app.controller.AppletParameters.getParametersInt("drawingId")
+    //If one was received, it is set as active drawing.
+    println("requestDrawingId:"+requestDrawingId)
+    if (requestDrawingId.isDefined)
+      com.siigna.app.controller.AppletParameters.setDrawingId(requestDrawingId.get)
 
     // Set the layout
     setLayout(new BorderLayout())
