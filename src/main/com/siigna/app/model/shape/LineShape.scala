bfa4384e44792f8a01bce5477b4e374f120aa23b
Jegp <jensep@gmail.com>
2012-04-28 17:39:02 +0200

    Renamed ShapePart to ShapeSelector did a helluva lot of changes...

--- src/main/com/siigna/app/model/shape/LineShape.scala
+++ src/main/com/siigna/app/model/shape/LineShape.scala
@@ -36,40 +36,49 @@ case class LineShape(p1 : Vector2D, p2 : Vector2D, attributes : Attributes) exte
 
   val points = Iterable(p1, p2)
 
-  def apply(part : ShapePart) = part match {
-    case SmallShapePart(1) => Some(new PartialShape((t : TransformationMatrix) => LineShape(p1.transform(t), p2, attributes)))
-    case SmallShapePart(2) => Some(new PartialShape((t : TransformationMatrix) => LineShape(p1, p2.transform(t), attributes)))
-    case FullShapePart => Some(new PartialShape(transform))
+  val start = p1
+
+  def apply(part : ShapeSelector) = part match {
+    case SmallShapeSelector(1) => Some(new PartialShape((t : TransformationMatrix) => LineShape(p1.transform(t), p2, attributes)))
+    case SmallShapeSelector(2) => Some(new PartialShape((t : TransformationMatrix) => LineShape(p1, p2.transform(t), attributes)))
+    case FullShapeSelector => Some(new PartialShape(transform))
     case _ => None
   }
 
-  def select(r : Rectangle2D) = {
-    if (r.contains(boundary)) {
-      if (r.contains(p1)) {
-        SmallShapePart(1)
-      } else if (r.contains(p2)) {
-        SmallShapePart(2)
-      } else EmptyShapePart
-    } else EmptyShapePart
+  def delete(part : ShapeSelector) : Option[Shape] = part match {
+    case SmallShapeSelector(_) | FullShapeSelector => None
+    case _ => Some(this)
   }
-  
-  def select(p : Vector2D) = {
+
+  def getPart(r : Rectangle2D) = {
+    if (r.intersects(boundary)) {
+      if (r.contains(p1) && r.contains(p2)) {
+        FullShapeSelector
+      } else if (r.contains(p1)) {
+        SmallShapeSelector(1)
+      } else if (r.contains(p2)) {
+        SmallShapeSelector(2)
+      } else EmptyShapeSelector
+    } else EmptyShapeSelector
+  }
+
+  def getPart(p : Vector2D) = {
     if (distanceTo(p) > Preferences.double("selectionDistance")) {
-      EmptyShapePart
+      EmptyShapeSelector
     } else {
       if (p.distanceTo(p1) < p.distanceTo(p2)) {
-        SmallShapePart(1)
+        SmallShapeSelector(1)
       } else {
-        SmallShapePart(2)
+        SmallShapeSelector(2)
       }
     }
   }
 
-  val start = p1
-  
-  def delete(part : ShapePart) : Option[Shape] = part match {
-    case SmallShapePart(_) | FullShapePart => None
-    case _ => Some(this)
+  def getVertices(selector: ShapeSelector) = selector match {
+    case FullShapeSelector => geometry.vertices
+    case SmallShapeSelector(1) => Seq(p1)
+    case SmallShapeSelector(2) => Seq(p2)
+    case _ => Seq()
   }
 
   def setAttributes(attributes : Attributes) = LineShape(p1, p2, attributes)
