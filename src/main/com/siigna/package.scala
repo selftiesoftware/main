52b062da99701b3c98d36c992bf02221b5b30af2
Jegp <jensep@gmail.com>
2012-05-30 11:42:47 +0200

    Removed AppletParameters from the package object

--- src/main/com/siigna/package.scala
+++ src/main/com/siigna/package.scala
@@ -34,7 +34,6 @@ package object siigna extends com.siigna.util.Implicits {
 
   // Controller
   lazy val Controller = com.siigna.app.controller.Controller
-  val AppletParameters = com.siigna.app.controller.AppletParameters
 
   // Model
   val Model = com.siigna.app.model.Model
