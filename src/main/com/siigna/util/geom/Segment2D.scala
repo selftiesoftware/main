393b93dd7351c5428a812d9c43fb00deff0d8daa
Ole Egholm <oleegholm@gmail.com>
2012-07-19 10:33:14 +0200

    Refactored geometry-classes to have one single method per operation, instead of overloading for all geometry classes.

--- src/main/com/siigna/util/geom/Segment2D.scala
+++ src/main/com/siigna/util/geom/Segment2D.scala
@@ -43,22 +43,13 @@ case class Segment2D(p1 : Vector2D, p2 : Vector2D) extends GeometryBasic2D with 
 
   def boundary = Rectangle(p1, p2)
 
-  def distanceTo(arc : Arc2D) = POSITIVE_INFINITY
-  def distanceTo(circle : Circle2D) = POSITIVE_INFINITY
-  def distanceTo(ellipse : Ellipse2D) = POSITIVE_INFINITY
-  def distanceTo(line : Line2D) = POSITIVE_INFINITY
-  def distanceTo(rectangle : Rectangle2D) = POSITIVE_INFINITY
-  def distanceTo(segment : Segment2D) = POSITIVE_INFINITY
-
-  /**
-   * Calculates the distance between a vector and the line segment
-   * TODO: Split this up for optimization.
-   */
-  def distanceTo(point : Vector2D) : Double = (point - closestPoint(point)).length
+  def distanceTo(geom  : Geometry2D) = geom match {
+    case point : Vector2D => (point - closestPoint(point)).length
+    case g => throw new UnsupportedOperationException("Segment: distanceTo not yet implemented with " + g)
+  }
 
   /**
    * Calculates the closest point from a point to this line.
-   * TODO: Split the function up, so distanceToPoint uses a more optimized algorithm.
    */
   def closestPoint(point : Vector2D) = {
     // The point can be either 'outside' start or end. Therefore we have to
@@ -98,124 +89,72 @@ case class Segment2D(p1 : Vector2D, p2 : Vector2D) extends GeometryBasic2D with 
   /**
    * Determines whether this segment intersects with a given Arc.
    */
-  def intersects(arc : Arc2D) = false
-  def intersects(circle : Circle2D) : Boolean = false
-  def intersects(ellipse : Ellipse2D) : Boolean = false
-  def intersects(rect : Rectangle2D) : Boolean = false
-  def intersects(line : Line2D) = false
+  def intersects(geom : Geometry2D) = geom match {
+    /**
+     * Determines whether two line segments intersect.
+     */
+    case line : Segment2D => {
+      // The line segments are defined by the equations:
+      //   L1 = A*u + C
+      //   L2 = B*v + D
+      val A = p2 - p1
+      val B = line.p2 - line.p1
+      val C = p1
+      val D = line.p1
+      // To find the intersection, set the two above equations equal:
+      //   L1 = L2  <=>  A*u + C = B*v + D
+      // The intersection is defined by solving (u,v) in the linear system:
+      //   A*u - B*v = D - C
+      // Calculate the determinant:
+      //   det = | A  -B | = | B  A |
+      // If det = 0 there are no solutions (the lines are parallel). Now we find
+      // the solutions:
+      //   u = | (D - C)  -B | / det = | B  (D - C) | / det
+      //   v =                         | A  (D - C) | / det
+      // The way the segments are defined u and v must be in the range 0...1
+      // (both inclusive), else the intersection is "outside" the line segments.
+      val det = Vector2D.determinant(B, A)
+      val uNotDivided = Vector2D.determinant(B, D - C)
+      val vNotDivided = Vector2D.determinant(A, D - C)
 
-  /**
-   * Determines whether two line segments intersect.
-   */
-  def intersects(line : Segment2D) : Boolean =
-  {
-    // The line segments are defined by the equations:
-    //   L1 = A*u + C
-    //   L2 = B*v + D
-    val A = p2 - p1
-    val B = line.p2 - line.p1
-    val C = p1
-    val D = line.p1
-    // To find the intersection, set the two above equations equal:
-    //   L1 = L2  <=>  A*u + C = B*v + D
-    // The intersection is defined by solving (u,v) in the linear system:
-    //   A*u - B*v = D - C
-    // Calculate the determinant:
-    //   det = | A  -B | = | B  A |
-    // If det = 0 there are no solutions (the lines are parallel). Now we find
-    // the solutions:
-    //   u = | (D - C)  -B | / det = | B  (D - C) | / det
-    //   v =                         | A  (D - C) | / det
-    // The way the segments are defined u and v must be in the range 0...1
-    // (both inclusive), else the intersection is "outside" the line segments.
-    val det = Vector2D.determinant(B, A)
-    val uNotDivided = Vector2D.determinant(B, D - C)
-    val vNotDivided = Vector2D.determinant(A, D - C)
-    
-    // The determinant must not be 0, and 0 <= u <= 1 and 0 <= v <= 1.
-    (det != 0 && between0And1(uNotDivided, det) && between0And1(vNotDivided, det))
+      // The determinant must not be 0, and 0 <= u <= 1 and 0 <= v <= 1.
+      (det != 0 && between0And1(uNotDivided, det) && between0And1(vNotDivided, det))
+    }
+    case g => throw new UnsupportedOperationException("Segment: intersects not yet implemented with " + g)
   }
 
   /**
    * Returns a list of points where a the Line Segment intersects with a given arc.
    */
-  def intersections(arc : Arc2D) : Set[Vector2D] =
-    intersections(Circle(arc.center, arc.radius)).filter(p => arc.insideArcDegrees((p - arc.center).angle))
+  def intersections(geom : Geometry2D) : Set[Vector2D] = geom match {
+    // TODO: Do this properly
+    case arc : Arc2D => intersections(Circle(arc.center, arc.radius)).filter(p => arc.insideArcDegrees((p - arc.center).angle))
+    /**
+     * Returns a list of points, defined as the intersection(s) between the
+     * segment and the circle.
+     */
+    case circle : Circle2D => {
+      val f           = p2 - p1
+      val g           = p1 - circle.center
+      val determinant = (f * g) * (f * g) - (f * f) * (g * g - circle.radius * circle.radius)
 
-  /**
-   * Returns a list of points, defined as the intersection(s) between the
-   * segment and the circle.
-   */
-  def intersections(circle : Circle2D) : Set[Vector2D] =
-  {
-    val f           = p2 - p1
-    val g           = p1 - circle.center
-    val determinant = (f * g) * (f * g) - (f * f) * (g * g - circle.radius * circle.radius)
-    
-    val t =
-      if (determinant < 0)
-        Set()
-      else if (determinant > 0)
-        Set((-(f * g) - scala.math.sqrt(determinant)) / (f * f),
-             (-(f * g) + scala.math.sqrt(determinant)) / (f * f))
-      else
-        Set(-(f * g) / (f * f))
+      val t =
+        if (determinant < 0)
+          Set()
+        else if (determinant > 0)
+          Set((-(f * g) - scala.math.sqrt(determinant)) / (f * f),
+               (-(f * g) + scala.math.sqrt(determinant)) / (f * f))
+        else
+          Set(-(f * g) / (f * f))
 
-    // Filter out the points, that isn't on the line-segment.
-    t.map(f * _ + p1).filter( point =>
-      ((p2 - p1) * (point - p1) >= 0 &&
-       (p1 - p2) * (point - p2) >= 0)
-    )
-  }
+      // Filter out the points, that isn't on the line-segment.
+      t.map(f * _ + p1).filter( point =>
+        ((p2 - p1) * (point - p1) >= 0 &&
+         (p1 - p2) * (point - p2) >= 0)
+      )
+    }
 
-  def intersections(ellipse : Ellipse2D) = Set()
-
-  /**
-   * Returns a list of points, defined as the intersection(s) between the
-   * segment and the rectangle.
-   */
-  def intersections(rectangle : Rectangle2D) = Set()
-
-  /**
-   * Returns a list of points, defined as the intersection(s) between the line-segment and the line.
-   * TODO!
-   */
-  def intersections(line : Line2D) = Set()
-
-  /**
-   * Returns a list of points, defined as the intersection between this segment and that segment.
-   */
-  def intersections(line : Segment2D) = {
-    // The line segments are defined by the equations:
-    //   L1 = A*u + C
-    //   L2 = B*v + D
-    val A = p2 - p1
-    val B = line.p2 - line.p1
-    val C = p1
-    val D = line.p1
-    // To findRecursive the intersection, set the two above equations equal:
-    //   L1 = L2  <=>  A*u + C = B*v + D
-    // The intersection is defined by solving (u,v) in the linear system:
-    //   A*u - B*v = D - C
-    // Calculate the determinant:
-    //   det = | A  -B | = | B  A |
-    // If det = 0 there are no solutions (the lines are parallel).
-    val det = Vector2D.determinant(B, A)
-    // The way the segments are defined u and v must be in the range 0...1
-    // (both inclusive), else the intersection is "outside" the line segments.
-    val uNotDivided = Vector2D.determinant(B, D - C)
-    val vNotDivided = Vector2D.determinant(A, D - C)
-
-    // Test the conditions above.
-    if (det != 0 && between0And1(uNotDivided, det) && between0And1(vNotDivided, det)) {
-      // Now we find the solution for u:
-      //   u = | (D - C)  -B | / det = | B  (D - C) | / det
-      val u = uNotDivided / det
-      // And use it in the equation L1 = A*u + C
-      val intersection = A*u + C
-      // Return the intersection.
-      Set(intersection)
-    } else Set() // No intersection
+    case g => throw new UnsupportedOperationException("Segment: intersections not yet implemented with " + g)
   }
   
   def transform(t : TransformationMatrix) = new Segment2D(t.transform(p1), t.transform(p2))
