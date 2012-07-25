07e6af3750b7e9a78f3f54cf1e4d1f3052d74b82
Jegp <jensep@gmail.com>
2012-07-25 17:11:43 +0200

    When modules throw errors in the controller they are now stopped.

--- src/main/com/siigna/app/controller/Controller.scala
+++ src/main/com/siigna/app/controller/Controller.scala
@@ -18,6 +18,7 @@ import com.siigna.module.Module
 import com.siigna.util.logging.Log
 import remote._
 import com.siigna.app.model.action.Action
+import com.siigna.app.view.View
 
 /**
  * The Controller controls the core of the software. Basically that includes
@@ -89,6 +90,9 @@ object Controller extends CommandController {
         // Unknown
         case e => Log.warning("Controller: Received unknown input: " + e)
       }
+
+      // Draw the view
+      View.repaint()
     }
   }
 
