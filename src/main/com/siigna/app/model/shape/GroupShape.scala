c92ec0e9bffb0fe25d9b689d7c86914b41282c13
Jegp <jensep@gmail.com>
2012-07-25 22:13:02 +0200

    Added a getShape method to shapes that returns the selected part as an Option[Shape] given a Selector. Fixed issue #24 by removing the .par (setColor in Graphics is not thread safe). View now only draws the selected sub-parts of shapes instead of the entire shape. Moved the drawing of the current selection into mainline.

--- src/main/com/siigna/app/model/shape/GroupShape.scala
+++ src/main/com/siigna/app/model/shape/GroupShape.scala
@@ -60,12 +60,22 @@ case class GroupShape(shapes : Seq[Shape], attributes : Attributes) extends Coll
   def getPart(point: Vector2D) = {
     shapes.reduceLeft((a : Shape, b : Shape) => if (a.distanceTo(point) <= b.distanceTo(point)) a else b).getPart(point)
   }
+  
+  def getShape(s : ShapeSelector) = s match {
+    case FullSelector => Some(this)
+    case GroupShape.Selector(xs) => {
+      Some(copy(shapes = xs.map(t => shapes(t._1).getShape(t._2)).collect {
+        case Some(s : Shape) => s
+      }.toSeq))
+    }
+    case _ => None
+  }
 
   def getVertices(selector: ShapeSelector) = shapes.flatMap(_.getVertices(selector))
 
-  def join(shape: Shape) = throw new UnsupportedOperationException("Not yet implemented")
+  def join(shape : Shape) = copy(shapes = shapes :+ shape)
 
-  def join(shapes: Traversable[Shape]) = throw new UnsupportedOperationException("Not yet implemented")
+  def join(shapes: Traversable[Shape]) = copy(shapes = this.shapes ++ shapes)
 
   /**
    * Returns a new collection with a new set of attributes. In other words return a collection with a new id,
@@ -84,6 +94,7 @@ case class GroupShape(shapes : Seq[Shape], attributes : Attributes) extends Coll
  */
 object GroupShape {
 
+  // TODO: There must be a smarter way to do this instead of spending one class-reference per selected object.
   sealed case class Selector(selectors : Map[Int, ShapeSelector]) extends ShapeSelector
 
   def apply(shapes : Traversable[Shape]) = new GroupShape(shapes.toSeq, Attributes())
