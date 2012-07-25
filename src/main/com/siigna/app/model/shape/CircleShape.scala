16e902f260c96237474411d6cf68c06378c4c8c4
Jegp <jensep@gmail.com>
2012-07-25 22:13:02 +0200

    Added a getShape method to shapes that returns the selected part as an Option[Shape] given a Selector. Fixed issue #24 by removing the .par (setColor in Graphics is not thread safe). View now only draws the selected sub-parts of shapes instead of the entire shape. Moved the drawing of the current selection into mainline.

--- src/main/com/siigna/app/model/shape/CircleShape.scala
+++ src/main/com/siigna/app/model/shape/CircleShape.scala
@@ -67,6 +67,11 @@ case class CircleShape(center : Vector2D, radius : Double, attributes : Attribut
     else {
       FullSelector
     }
+  
+  def getShape(s : ShapeSelector) = s match {
+    case FullSelector => Some(this)
+    case _ => None
+  }
 
   def getVertices(selector: ShapeSelector) = selector match {
     case FullSelector => geometry.vertices
