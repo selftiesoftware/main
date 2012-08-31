da37d0f54ca238ede2d578e5732e0e1b9699f392
Christoffer Pedersen <stofferpedersen@gmail.com>
2012-08-31 14:52:41 +0200

    removed hardcoding

--- src/main/com/siigna/app/controller/remote/RemoteController.scala
+++ src/main/com/siigna/app/controller/remote/RemoteController.scala
@@ -53,7 +53,6 @@ protected[controller] object RemoteController {
   protected val remote = select(Node("localhost", 20004), 'siigna)
 
   val SiignaDrawing = com.siigna.app.model.Drawing // Use the right namespace
-  SiignaDrawing.addAttribute("id",20L)
   // The local sink, receiving actions from the remote sink
   protected val local : Actor = actor {
 
