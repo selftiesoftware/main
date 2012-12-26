371c18b9d37332dfb5d9a75ad62c1c9c1c3c10bd
jegp <jensep@gmail.com>
2012-12-26 09:44:22 +0100

    Commented

--- src/main/com/siigna/app/controller/remote/RemoteController.scala
+++ src/main/com/siigna/app/controller/remote/RemoteController.scala
@@ -56,7 +56,6 @@ protected[controller] object RemoteController extends Actor {
   def act() {
     try {
 
-      SiignaDrawing.addAttribute("id" -> 38L)
       def drawingId : Option[Long] = SiignaDrawing.attributes.long("id")
 
       // If we have a drawing we need to fetch it if we don't we need to reserve it
