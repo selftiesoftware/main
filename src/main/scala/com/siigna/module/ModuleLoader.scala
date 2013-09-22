8eb2d4e94a2303fecb27a08e381b5717a83d955d
Niels <nielsegholm@gmail.com>
2013-09-22 14:02:11 +0200

    ass

--- src/main/scala/com/siigna/module/ModuleLoader.scala
+++ src/main/scala/com/siigna/module/ModuleLoader.scala
@@ -57,8 +57,6 @@ object ModuleLoader {
   final val modulePath = "com.siigna.module"
 
   // Create a default packages
-  future {
-
     load(ModulePackage('base, s"rls.siigna.com/com/siigna/siigna-base_2.10/$stability", s"siigna-base_2.10-$stability.jar", local = false))
     load(ModulePackage('cad, s"rls.siigna.com/com/siigna/siigna-cad-suite_2.10/$stability", s"siigna-cad-suite_2.10-$stability.jar", local = false))
     load(ModulePackage('porter, s"rls.siigna.com/com/siigna/siigna-porter_2.10/$stability", s"siigna-porter_2.10-$stability.jar", local = false))
@@ -75,7 +73,6 @@ object ModuleLoader {
     //load(ModulePackage('base, "c:/siigna/main/out/artifacts", "base.jar", true))
     //load(ModulePackage('cad, "c:/siigna/main/out/artifacts", "cad-suite.jar", true))
     //load(ModulePackage('porter, "c:/siigna/main/out/artifacts", "porter.jar", true))
-  }
 
 
   /**
