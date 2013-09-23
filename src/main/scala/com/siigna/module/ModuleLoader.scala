30eb4a9d80112dfc2147b23f1d1a5b3417b985d2
Niels <nielsegholm@gmail.com>
2013-09-23 17:25:53 +0200

    Removing debug-println

--- src/main/scala/com/siigna/module/ModuleLoader.scala
+++ src/main/scala/com/siigna/module/ModuleLoader.scala
@@ -65,7 +65,6 @@ object ModuleLoader {
     load(ModulePackage('cad, s"rls.siigna.com/com/siigna/siigna-cad-suite_2.10/$stability", s"siigna-cad-suite_2.10-$stability.jar", local = false))
     load(ModulePackage('porter, s"rls.siigna.com/com/siigna/siigna-porter_2.10/$stability", s"siigna-porter_2.10-$stability.jar", local = false))
     modulesLoaded = true
-    println("Modules loaded!!!")
 
     //load(ModulePackage('base, "c:/workspace/siigna/main/out/artifacts", "base.jar", true))
     //load(ModulePackage('cad, "c:/workspace/siigna/main/out/artifacts", "cad-suite.jar", true))
